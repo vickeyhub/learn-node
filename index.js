b5695b72b35f0971adb721cf0757ee3c88284213
vishal maurya <vishalweb9@gmail.com>
2023-04-19 15:40:03 +0530

    checking the diffrent routes methods with express JS

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
+    res.json({type: 'GET'});
+});
+server.post('/', (req, res) => {
+    res.json({type: 'POST'});
+});
+
+server.put('/', (req, res) => {
+    res.json({type:'PUT'})
+});
+server.delete('/', (req, res) => {
+    res.json({type:'DELETE'})
+});
+server.patch('/', (req, res) => {
+    res.json({type:'PATCH'})
+});
+
+server.get('/demo', (req, res) => {
     // res.sendStatus(404);
     res.status(200).send('<h1>hello</h1>');
     // res.sendFile('C:/MERN/learn-node/index.html');
