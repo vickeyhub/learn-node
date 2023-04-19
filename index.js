4983b99ccb289903bc0e65f2d58ff375392da74a
vishal maurya <vickey.shopism@gmail.com>
2023-04-19 13:41:45 +0530

    create a product parameter for a call the single product data via URL parameters in node without express js

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const products = data.products;
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method)
+    if(req.url.startsWith('/product')){
+        const id = req.url.split('/')[2]
+        const product = products.find(p=>p.id === (+id))
+        res.setHeader('Content-Type', 'text/html')
+            let modifiedIndex = index.replace('**title**', product.title)
+                .replace('**description**', product.description)
+                .replace('**url**', product.thumbnail)
+                .replace('**price**', product.price)
+            res.end(modifiedIndex);
+            return;
+    }
     switch (req.url) {
         case '/':
             res.setHeader('Content-Type', 'text/html')
@@ -19,14 +30,14 @@ const server = http.createServer((req, res) => {
             res.end(products);
             break;
 
-        case '/product':
-            res.setHeader('Content-Type', 'text/html')
-            let modifiedIndex = index.replace('**title**', product.title)
-                .replace('**description**', product.description)
-                .replace('**url**', product.thumbnail)
-                .replace('**price**', product.price)
-            res.end(modifiedIndex);
-            break;
+        // case '/product':
+        //     res.setHeader('Content-Type', 'text/html')
+        //     let modifiedIndex = index.replace('**title**', product.title)
+        //         .replace('**description**', product.description)
+        //         .replace('**url**', product.thumbnail)
+        //         .replace('**price**', product.price)
+        //     res.end(modifiedIndex);
+        //     break;
         default:
             res.writeHead(404, 'Not Found');
             res.end();
