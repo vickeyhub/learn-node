d3bbe20ab46d749167554576ac11d964ead08e5b
vishal maurya <vishalweb9@gmail.com>
2023-04-19 13:56:44 +0530

    install erxpress JS and write custom script in package.json for run the application easily

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
-const http = require('http');
 const fs = require('fs');
+// const index = fs.readFileSync('index.html', 'utf-8');
+// const data = JSON.parse(fs.readFileSync('data/products.json', 'utf-8'));
+// const products = data.products;
 
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data/products.json', 'utf-8'));
+const express = require('express');
 
-const products = data.products;
-
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
-    if(req.url.startsWith('/product')){
-        const id = req.url.split('/')[2]
-        const product = products.find(p=>p.id === (+id))
-        res.setHeader('Content-Type', 'text/html')
-            let modifiedIndex = index.replace('**title**', product.title)
-                .replace('**description**', product.description)
-                .replace('**url**', product.thumbnail)
-                .replace('**price**', product.price)
-            res.end(modifiedIndex);
-            return;
-    }
-    switch (req.url) {
-        case '/':
-            res.setHeader('Content-Type', 'text/html')
-            res.end(index);
-            break;
-
-        case '/api':
-            res.setHeader('Content-Type', 'application/json')
-            res.end(products);
-            break;
-
-        // case '/product':
-        //     res.setHeader('Content-Type', 'text/html')
-        //     let modifiedIndex = index.replace('**title**', product.title)
-        //         .replace('**description**', product.description)
-        //         .replace('**url**', product.thumbnail)
-        //         .replace('**price**', product.price)
-        //     res.end(modifiedIndex);
-        //     break;
-        default:
-            res.writeHead(404, 'Not Found');
-            res.end();
-            break;
-
-    }
-
-
-});
+const server = express();
 server.listen(8080);
