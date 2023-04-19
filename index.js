88314f094b61ab5403389387e609194c59ab0465
vishal maurya <vickey.shopism@gmail.com>
2023-04-19 13:40:05 +0530

    initial upload of learning the node js with custom routing based server calling

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data/products.json', 'utf-8'));
+
+const products = data.products;
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url, req.method)
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html')
+            res.end(index);
+            break;
+
+        case '/api':
+            res.setHeader('Content-Type', 'application/json')
+            res.end(products);
+            break;
+
+        case '/product':
+            res.setHeader('Content-Type', 'text/html')
+            let modifiedIndex = index.replace('**title**', product.title)
+                .replace('**description**', product.description)
+                .replace('**url**', product.thumbnail)
+                .replace('**price**', product.price)
+            res.end(modifiedIndex);
+            break;
+        default:
+            res.writeHead(404, 'Not Found');
+            res.end();
+            break;
+
+    }
+
+
+});
+server.listen(8080);
