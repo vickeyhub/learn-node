ce6a904faecb0462b05c21f5e868ec8f81c8ca62
vishal maurya <vishalweb9@gmail.com>
2023-04-19 13:44:47 +0530

    checking the URL method via req.method in console

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const data = JSON.parse(fs.readFileSync('data/products.json', 'utf-8'));
 const products = data.products;
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method)
+    console.log(req.url, req.method)
     if(req.url.startsWith('/product')){
         const id = req.url.split('/')[2]
         const product = products.find(p=>p.id === (+id))
