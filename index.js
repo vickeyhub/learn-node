a3f17b0674c68f9ec50e1b38f927c0efa4c4ca3b
vishal maurya <vishalweb9@gmail.com>
2023-04-19 15:33:58 +0530

    checking the response methods with status

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 // const index = fs.readFileSync('index.html', 'utf-8');
-// const data = JSON.parse(fs.readFileSync('data/products.json', 'utf-8'));
-// const products = data.products;
+const data = JSON.parse(fs.readFileSync('data/products.json', 'utf-8'));
+const products = data.products;
 
 const express = require('express');
 
 const server = express();
-server.listen(8080);
+
+server.get('/', (req, res) => {
+    // res.sendStatus(404);
+    res.status(200).send('<h1>hello</h1>');
+    // res.sendFile('C:/MERN/learn-node/index.html');
+    // res.json(products);
+});
+server.listen(8080, () => {
+    console.log('Server started')
+});
